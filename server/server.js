24a509fff4487a16608cb87a8be19f9eb6c497c4
lofiandcode <josephtrettevik@gmail.com>
2023-01-03 18:05:56 -1000

    Create routes for GET, GET id, and POST
    Create route for GET all restaurants, GET a restaurant by id, and POST new restaurant.

--- server/server.js
+++ server/server.js
@@ -1,14 +1,84 @@
 require("dotenv").config();
 const express = require("express");
-
-
+const db = require("./db");
+const morgan = require('morgan');
 const app = express();
 
-app.get("/restaurants", (req, res) => {
-    res.json({
+app.use(express.json());
+
+//GET all Restaurants
+app.get("/api/v1/restaurants", async (req, res) => {
+    try {
+        const results = await db.query("SELECT * FROM restaurants");
+        console.log(results);
+        res.status(200).json({
+            status: "success",
+            results: results.rows.length,
+            data: {
+                restaurants: results.rows,
+            },
+        });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+// GET a Restaurant
+app.get("/api/v1/restaurants/:id", async (req, res) => {
+    console.log(req.params.id);
+
+    try {
+        const results = await db.query("SELECT * FROM restaurants WHERE id = $1", [req.params.id]);
+        res.status(200).json({
+            status: "success",
+            data: {
+                restaurant: results.rows[0],
+            }
+        });
+    } catch (error) {
+        console.log(error);
+    }
+
+});
+
+// Create a Restaurant
+app.post("/api/v1/restaurants", async (req,res) => {
+    console.log(req.body);
+
+    try {
+        const results = await db.query(
+            "INSERT INTO restaurants (name, location, price_range) VALUES ($1, $2, $3) returning *", 
+            [req.body.name, req.body.location. req.body.price_range]
+        );
+        res.status(201).json({
+            status: "success",
+            data: {
+                restaurant: results.rows[0],
+            }
+        });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+// Update a Restaurant
+app.put("/api/v1/restaurants/:id", (req, res) => {
+    console.log(req.params.id);
+    console.log(req.body);
+
+    res.status(200).json({
         status: "success",
-        restaurant: "McDonalds"
+        data: {
+            restaurant: "mcdonalds"
+        }
     });
+});
+
+//Delete a Restaurant
+app.delete("/api/v1/restaurants/:id", (req, res) => {
+    res.status(204).json({
+        status: "success"
+    })
 });
 
 const PORT = process.env.PORT || 3001;
